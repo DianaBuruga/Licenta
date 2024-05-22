28ed447a388ad447930e885f9b0d7958249f0e6d
DianaBuruga <100471189+DianaBuruga@users.noreply.github.com>
2024-05-22 23:27:06 +0300

    Error Response entity

--- src/main/java/com/ulbs/careerstartup/exception/ErrorResponse.java
+++ src/main/java/com/ulbs/careerstartup/exception/ErrorResponse.java
@@ -5,9 +5,9 @@ import java.time.Instant;
 import static com.ulbs.careerstartup.util.DateFormatter.formatDate;
 
 public class ErrorResponse {
-    private String message;
-    private int code;
-    private String dateTime;
+    private final String message;
+    private final int code;
+    private final String dateTime;
 
     public ErrorResponse(String message, int code) {
         this.message = message;
