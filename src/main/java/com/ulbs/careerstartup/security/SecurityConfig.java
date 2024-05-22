dacb2ae486d518a753414437869594693efcfd39
DianaBuruga <100471189+DianaBuruga@users.noreply.github.com>
2024-05-22 23:20:43 +0300

    Removed security for now. Added Cors config but does not work

--- src/main/java/com/ulbs/careerstartup/security/SecurityConfig.java
+++ src/main/java/com/ulbs/careerstartup/security/SecurityConfig.java
@@ -1,31 +1,24 @@
 package com.ulbs.careerstartup.security;
 
-import com.nimbusds.openid.connect.sdk.federation.policy.operations.DefaultOperation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -41,11 +34,11 @@ import static org.springframework.http.HttpHeaders.*;
         type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(
                 authorizationCode = @OAuthFlow(
-                        authorizationUrl = "{authorization-url}",
-                        tokenUrl = "{token-url}",
+                        authorizationUrl = "https://accounts.google.com/o/oauth2/auth?prompt=select_account",
+                        tokenUrl = "https://oauth2.googleapis.com/token",
                         scopes = {
-                                @OAuthScope(name = "read", description = "read access"),
-                                @OAuthScope(name = "write", description = "write access")
+                                @OAuthScope(name = "email", description = "email access"),
+                                @OAuthScope(name = "profile", description = "profile access")
                         }
                 )
         )
@@ -59,13 +52,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/oauth2/authorization/google-login").permitAll()
-                        .requestMatchers("/").permitAll()
-                        //.requestMatchers("/users/userinfo").permitAll()
+                        .requestMatchers("/oauth2/authorization/google-login", "/", "/auth/google", "/swagger-ui.html", "/error**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
@@ -77,59 +69,32 @@ public class SecurityConfig {
                         })
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                                .oidcUserService(oidcWorkspaceUserService)
-                        )
-//                                .successHandler((request, response, authentication) -> {
-//                                    response.addCookie(new Cookie("JSESSIONID", ((WebAuthenticationDetails)(authentication.getDetails())).getSessionId()));
-//                                    response.sendRedirect("http://localhost:4200/profile");
-//                                })
-                                            .defaultSuccessUrl("http://localhost:4200/profile")
-                                            .failureHandler(authenticationFailureHandler)
+                                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                        .oidcUserService(oidcWorkspaceUserService)
+                                )
+                                .failureHandler(authenticationFailureHandler)
 
-                        )
-        ;
-                //.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                )
+                .anonymous(AbstractHttpConfigurer::disable);
+        //.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        final CorsConfiguration config = new CorsConfiguration();
-////        config.setAllowCredentials(true);
-////        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8081", "https://accounts.google.com"));
-////        config.setAllowedHeaders(Arrays.asList(
-////                ORIGIN,
-////                CONTENT_TYPE,
-////                ACCEPT,
-////                AUTHORIZATION,
-////                ACCESS_CONTROL_ALLOW_ORIGIN
-////        ));
-////        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
-//        config.setAllowedOrigins(Arrays.asList("*"));
-//        config.setAllowedMethods(Arrays.asList("*"));
-//        config.setAllowedHeaders(Arrays.asList("*"));
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
-@Bean
-public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowCredentials(true);
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Specify the allowed origin
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(Arrays.asList(
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
                 ACCEPT,
                 AUTHORIZATION,
-                ACCESS_CONTROL_ALLOW_ORIGIN,
-                "x-auth-token"
+                ACCESS_CONTROL_ALLOW_ORIGIN
         ));
-    configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
